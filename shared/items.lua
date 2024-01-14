4cf94d4149fe6ee88cdde622b2edd49cb6cc0646
GG79 <midlifegamingcrisis@gmail.com>
2024-01-14 01:38:32 -0800

    added pocket bikes

--- shared/items.lua
+++ shared/items.lua
@@ -813,4 +813,13 @@ QBShared.Items = {
 
     --electric skateboard
     skateboard 		 	        = {name = 'skateboard', label = 'Skateboard', weight = 5, type = 'item', image = 'skateboard.png', unique = false, useable = true, shouldClose = true, combinable = nil, description = 'Skating Time!'},
+
+    -- pocket bikes
+    bmx                         = {name = "bmx", label = "BMX Bike", weight = 500, type = "item", image = "bmx.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A BMX bike"},
+    cruiser                     = {name = "cruiser", label = "Cruiser Bike", weight = 500, type = "item", image = "cruiser.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A Cruiser bike"},
+    fixter                      = {name = "fixter", label = "Fixter Bike", weight = 500, type = "item", image = "fixter.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A Fixter bike"},
+    scorcher                    = {name = "scorcher", label = "Scorcher Bike", weight = 500, type = "item", image = "scorcher.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A Scorcher bike"},
+    tribike                     = {name = "tribike", label = "Yellow TriBike", weight = 500, type = "item", image = "tribike.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A yellow Tribike"},
+    tribike2                    = {name = "tribike2", label = "Red TriBike", weight = 500, type = "item", image = "tribike2.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A red Tribike"},
+    tribike3                    = {name = "tribike3", label = "Blue TriBike", weight = 500, type = "item", image = "tribike3.png", unique = true, useable = true, shouldClose = true, combinable = nil, description = "A blue Tribike"},
 }
